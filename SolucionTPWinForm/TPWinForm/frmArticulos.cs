36307719457c1f5a1d0ae402229f1862f1d5ed03
SofiaAGayol <sofia.gayol@hotmail.com>
2024-09-14 22:12:17 -0300

    Primer commit

---
+++ SolucionTPWinForm/TPWinForm/frmArticulos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+
+namespace TPWinForm
+{
+    public partial class frmArticulos : Form
+    {
+        private List<Articulo> listaArticulo;
+        public frmArticulos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmArticulos_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void cargar()
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            try
+            {
+                listaArticulo = negocio.listar();
+                dataGridView.DataSource = listaArticulo;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+            
+        }
+    }
+}
---
+++ SolucionTPWinForm/negocio/ArticuloNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Id, Codigo, Nombre from ARTICULOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo articulo = new Articulo();
+                    articulo.Id = (int)lector["Id"];
+                    articulo.Codigo = (string)lector["Codigo"];
+                    articulo.Nombre = (string)lector["Nombre"];
+
+                    lista.Add(articulo); 
+                }
+
+                conexion.Close();
+                return lista;
+       
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+        
+}
